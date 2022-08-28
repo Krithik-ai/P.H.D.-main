f244d8be00ffee111863f999fa5c80ce9e63c1a6
Krithik Alluri <Krithik.alluri@gmail.com>
2022-08-28 15:28:33 -0400

    Add All Files

---
+++ AddNewDisease.py
@@ -0,0 +1,16 @@
+import pandas as pd
+df = pd.read_csv("archive/Training1.csv")
+is_hol = df.iloc[1366]
+
+print(is_hol.head(133))
+
+#ImmutableMultiDict([('symptoms', '~loss of smell~chills~muscle pain~muscle weakness~fatigue~cough~high fever~continuous sneezing')])
+#[[0. 0. 0. 1. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+#  1. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+ # 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+#  0. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0. 1. 0. 0. 0. 0. 0. 0. 0.
+#  0. 1. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+#  0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]]
+
+
+#df.to_csv('file1.csv')
